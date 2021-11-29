584ad59a164484b9e2e101717296ee3f17b5e57d
/\be| /\nand <90404118+AbelXAnand13@users.noreply.github.com>
2021-11-28 16:00:24 -0800

    Initial Commit

---
+++ classroomSimulator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  classroomSimulator
+//
+//  Created by Abel Christy Anand on 11/28/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
